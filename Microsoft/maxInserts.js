2523edb622f67cdf82a3ea77c541578f5b6e9fa3
Ronny Lin <ronny.lin@alumni.ubc.ca>
2019-12-25 14:50:05 -0800

    another easy one

---
+++ Microsoft/maxInserts.js
@@ -0,0 +1,25 @@
+/**
+ * 1. Iterate through string from first letter
+ * 2. Keep track of consecutive count of a, if above 3 then return -1
+ * 3. If current letter is not a, add 2-count of a infront of the letter or increment result count by the same number
+ * 4. After iteration finishes, add 2-count to result
+ * @param {*} word
+ */
+function solution (word) {
+  const arr = word.split('')
+  let count = 0
+  const length = arr.length
+  let maxInserts = 0
+  for (let i = 0; i < length; i++) {
+    if (arr[i] === 'a') {
+      count++
+      if (count > 2) return -1
+    } else {
+      maxInserts += 2 - count
+      count = 0
+    }
+  }
+  return maxInserts + 2 - count
+}
+
+console.log(solution('doga'))
