6e6bf8c796fe607a17c7f4e852a99dae091ec904
Ronny Lin <ronny.lin@alumni.ubc.ca>
2020-01-05 20:36:43 -0800

    test OA debug function

---
+++ Microsoft/OA.js
@@ -0,0 +1,9 @@
+function solution (A, K) {
+  var n = A.length
+  for (var i = 0; i < n - 1; i++) {
+    if (A[i] + 1 < A[i + 1]) { return false }
+  }
+  if (A[0] !== 1 || A[n - 1] !== K) { return false } else { return true }
+}
+
+console.log(solution([1, 2, 3, 3, 3, 3], 2))
